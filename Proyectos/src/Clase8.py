002d959208ea715eea445604edf3720a13c65b32
DaciBlue <150244411+DaciBlue@users.noreply.github.com>
2025-02-27 10:46:19 +0100

    enunciado Clase8.py

---
+++ Proyectos/src/Clase8.py
@@ -0,0 +1,11 @@
+'''
+• Implementar una interfaz gráfica en Python usando PyQt6 (PySide6).
+• Permitir al usuario agregar tareas a una lista.
+• Implementar la opción de eliminar tareas seleccionadas.
+• Escribir pruebas automatizadas con Pytest para verificar el correcto funcionamiento de la
+aplicación.
+• Crear un archivo README.md con la descripción del proyecto, instrucciones de instalación
+y uso.
+• Generar un archivo ejecutable utilizando PyInstaller para distribuir la aplicación sin
+necesidad de instalar dependencias.
+'''
